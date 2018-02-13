4e1cacd0963e3dc1fec74ec47783c52d079b9273
Ioannis Stamoulis <yanni.stamoulis@gmail.com>
2018-02-13 20:32:31 +0200

    Part1 v1.0

--- step1/verilog_input/Gray_4bits_RTL.v
+++ step1/verilog_input/Gray_4bits_RTL.v
@@ -1,9 +1,9 @@
-                                              -
+
  /*
  -----------------------------------------------------------------------------
  -- File           : Gray_4bits_RTL.v
  -----------------------------------------------------------------------------
- -- Description    : Verilog code for the 4-bit Gray 
+ -- Description    : Verilog code for the 4-bit Gray
  --  with asynchronous reset
  -- 4 bit gray code :
   --  bin     hex
@@ -24,17 +24,43 @@
   --  1001    9
   --  1000    8
  -- --------------------------------------------------------------------------
- */ 
+ */
 
-module gray_4bits (clk, clk_en, rst, gray_out)
-  input clk, clk_en, rst;
-  reg [3:0] state;
-  wire gray_out;
+module gray_4bits (clk, /*clk_en,*/ reset, gray_out);
+input wire clk, /*clk_en,*/ reset;
+output wire [3:0]gray_out;
 
-  always @(posedge clk or rst) 
-   begin
-     // Code to implement a 4-bit Gray code
-     
-    assign gray_out = state;
-  
-end module
+reg [3:0]state;
+
+assign gray_out = state;
+
+always @(posedge clk or posedge reset)
+begin
+	// Code to implement a 4-bit Gray code
+	if (reset == 1) begin
+		state <= 4'b0000;
+	end
+	else begin
+		case (gray_out)
+			4'b0000: state <= 4'b0001;
+			4'b0001: state <= 4'b0011;
+			4'b0011: state <= 4'b0010;
+			4'b0010: state <= 4'b0110;
+			4'b0110: state <= 4'b0111;
+			4'b0111: state <= 4'b0101;
+			4'b0101: state <= 4'b0100;
+			4'b0100: state <= 4'b1100;
+			4'b1100: state <= 4'b1101;
+			4'b1101: state <= 4'b1111;
+			4'b1111: state <= 4'b1110;
+			4'b1110: state <= 4'b1010;
+			4'b1010: state <= 4'b1011;
+			4'b1011: state <= 4'b1001;
+			4'b1001: state <= 4'b1000;
+			4'b1000: state <= 4'b0000;
+			default: state <= 4'b0000;
+		endcase
+	end
+end
+
+endmodule
--- step1/verilog_input/TB1_Gray.v
+++ step1/verilog_input/TB1_Gray.v
@@ -1,39 +1,42 @@
- /*                                -
- -----------------------------------------------------------------------------
- -----------------------------------------------------------------------------
- -- File           : TB1_Gray.v
- -----------------------------------------------------------------------------
- -- Description    : Verilog testbench for Gray_4bits
- -- --------------------------------------------------------------------------
- -- --------------------------------------------------------------------------
- */
- 
-`timescale 1ns/1ps
-`define cycle 10
+/*                                -
+-----------------------------------------------------------------------------
+-----------------------------------------------------------------------------
+-- File           : TB1_Gray.v
+-----------------------------------------------------------------------------
+-- Description    : Verilog testbench for Gray_4bits
+-- --------------------------------------------------------------------------
+-- --------------------------------------------------------------------------
+*/
 
-module TB1;
-  
-  reg clk, rst, clk_en;
-  reg [3:0] leds;
-  integer EndOfSimulation;  
-  integer i;
 
-  parameter ONE = 1'b1; 
-							 
-  // Initial statement for signal initialization (reset, clk, EndOfSimulation)
-   initial 
-     begin
-      // ....
-	  $finish
-     end;
-	 
-  // Always statement to drive the clock goes here
-  always 
-    begin
-	// ...
-    end
-	
-   // Instantiation of the gray_4bits 
-  gray_4bits DUT(.clk(clk), .clk_en(ONE), .rst(rst), .gray_out(leds));   
-  
-end module;
+module TB1();
+
+reg clk, reset /*, clk_en*/;
+wire [3:0]leds;
+//integer EndOfSimulation;
+//integer i;
+
+//parameter ONE = 1'b1;
+
+// Instantiation of the gray_4bits
+gray_4bits DUT(
+	.clk		(clk),
+	//.clk_en		(ONE),
+	.reset		(reset),
+	.gray_out	(leds)
+);
+
+// Initial statement for signal initialization (reset, clk, EndOfSimulation)
+initial begin
+	clk <= 1;
+	reset <= 1;
+	#20reset <= 0;
+	#300$finish;
+end
+
+// Always statement to drive the clock goes here
+always begin
+	#5 clk <= ~clk;
+end
+
+endmodule
